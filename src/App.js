23f3f8b33f27262287194af47260cc4404fd870a
nchernys <natasha.e.chernysheva@gmail.com>
2024-05-14 13:56:47 -0400

    edit insta deployment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import BinaryTree from "./binaryTree";
 import Blog from "./allBlog";
 import Queue from "./queueDataStructure";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import InstagramFeed from "./instagramFeedAPI";
 
 const App = () => {
   return (
@@ -13,7 +12,6 @@ const App = () => {
         <Route path="/blog-binary-search" element={<BinaryTree />} />
         <Route path="/blog-bubble-sort" element={<BubbleSort />} />
         <Route path="/blog-queue-structure" element={<Queue />} />
-        <Route path="/blog-instagram-feed" element={<InstagramFeed />} />
       </Routes>
     </Router>
   );
